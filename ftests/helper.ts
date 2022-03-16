42f1bae15b0f9288a9e73334c28166c1124ff4e2
imshaiknasir <skfiroj1994@gmail.com>
2022-03-16 23:29:41 +0530

    day 2

--- ftests/helper.ts
+++
@@ -1,7 +0,0 @@
-export async function locadGooglePage(page) {
-    await page.goto('https://google.com/')
-}
-
-export async function takeFullpageScreenshot(page) {
-    await page.screenshot({ path: "screenshot.png" , fullPage: true })
-}
